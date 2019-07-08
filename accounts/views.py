f7e592c4683e5f24f73bca9a98503c987e5c190f
Milosz Sobiczewski <msobiczewski@Milosz-Sobiczewski.local>
2019-07-08 20:37:00 +0200

    initial accounts managements app setup

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/accounts/login/')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('/accounts/')
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {'form': form})
