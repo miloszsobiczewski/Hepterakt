678bf1482e200364ccd59bb1fad3b6634167f0c8
Milosz-mrx <m.sobiczewski@merixstudio.com>
2019-10-10 14:19:14 +0200

    hide LoginRequiredMiddleware

--- Hepterakt/settings.py
+++ Hepterakt/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Hepterakt.middleware.LoginRequiredMiddleware',
+    # 'Hepterakt.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'Hepterakt.urls'
